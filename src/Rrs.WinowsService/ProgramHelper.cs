64709bde15a585a0a998991e51a9c43b713d0acd
Robert Snipe <robertjsnipe@gmail.com>
2019-12-30 15:37:26 +0000

    initial

---
+++ src/Rrs.WinowsService/ProgramHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Rrs.WinowsService
+{
+    public static class ProgramHelper
+    {
+        public static int Run(ServiceBase service, string[] args, string serviceDisplayName = null, string serviceDescription = null)
+        {
+            try
+            {
+                if (args.Length > 0)
+                {
+                    if (args[0] == "--service")
+                    {
+                        ServiceBase.Run(new ServiceBase[]
+                        {
+                            service
+                        });
+                    }
+                    else
+                    {
+                        ConsoleHelper.Run(service.ServiceName, args, serviceDisplayName, serviceDescription);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("install with\t-i");
+                    Console.WriteLine("uninstall with\t-u");
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+    }
+}
---
+++ src/Rrs.WinowsService/ServiceAppInstaller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+
+namespace Rrs.WinowsService
+{
+    [RunInstaller(true)]
+    public class ServiceAppInstaller : Installer
+    {
+        public static void Install(string serviceName, bool undo, string[] args, string serviceDisplayName = null, string description = null)
+        {
+            try
+            {
+                Console.WriteLine(undo ? "uninstalling" : "installing");
+                using (var inst = new AssemblyInstaller(Assembly.GetEntryAssembly(), args))
+                {
+                    var processInstaller = new ServiceProcessInstaller
+                    {
+                        Account = ServiceAccount.LocalSystem
+                    };
+
+                    var serviceInstaller = new System.ServiceProcess.ServiceInstaller
+                    {
+                        StartType = ServiceStartMode.Automatic,
+                        ServiceName = serviceName,
+                        DisplayName = serviceDisplayName ?? Regex.Replace(serviceName, "(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])", " "),
+                        Description = description
+                    };
+
+                    inst.Installers.Add(processInstaller);
+                    inst.Installers.Add(serviceInstaller);
+
+                    IDictionary state = new Hashtable();
+                    inst.UseNewContext = true;
+                    try
+                    {
+                        if (undo)
+                        {
+                            var sc = new ServiceController(serviceName);
+                            if (sc.Status == ServiceControllerStatus.Running) sc.Stop();
+                            inst.Uninstall(state);
+                        }
+                        else
+                        {
+                            inst.BeforeInstall += (s, e) => { inst.Context.Parameters["assemblyPath"] = $@"""{inst.Context.Parameters["assemblyPath"]}"" --service"; };
+                            inst.AfterInstall += (s, e) => { new ServiceController(serviceName).Start(); };
+                            inst.Install(state);
+                            inst.Commit(state);
+                        }
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            inst.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
